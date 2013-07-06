bca69a133d81dc7596c40966d975a2bbd0c6dbcb
MaximusDecimusMeridius <kiowa.jackson@yahoo.com>
2013-07-06 07:09:10 +0000

    read from index

--- web.js
+++ web.js
@@ -2,7 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var text = "yoooo"
+
+var text = fs.readFileSync("index.html");
+
+var content = text.toString("utf-8",0,text.length-1);
+
 
 app.get('/', function(request, response) {
   response.send(text);
