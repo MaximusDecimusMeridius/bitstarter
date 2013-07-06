6761ff01977ba3b9760046d99f2e5339dfcda704
MaximusDecimusMeridius <kiowa.jackson@yahoo.com>
2013-07-06 06:34:08 +0000

    wtff

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var test = "yoooo";
+//var test = "yoooo";
 //var html = fs.readFileSync(index.html)
 
 // var text = html.toString(String)
